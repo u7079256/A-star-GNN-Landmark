898857cda5520bd755202ca1e2a0d217b730a61e
u7079256 <wyrsjz@163.com>
2022-12-23 00:02:17 +1100

    simple implementation of Dijkstra's Algorithm

--- a_star_algo.py
+++ a_star_algo.py
@@ -1,6 +1,6 @@
 import numpy as np
 import os
-def dijkstra(adj_matrix, from_node, to_node):
+def dijkstra_one_to_one(adj_matrix, from_node, to_node):
     """
     Basic Dijkstra algo implementation
     :param adj_matrix: adj matrix which contains information related to the graph
@@ -12,6 +12,27 @@ def dijkstra(adj_matrix, from_node, to_node):
     distance_stack = [np.inf for i in range(len(adj_matrix))]
     distance_stack[from_node] = 0
     while close_set[to_node] == 0:
+        next_node = minimum_node(distance_stack,np.inf,close_set)
+        connection_status = adj_matrix[next_node]
+        for i in range(len(connection_status)):
+            if connection_status[i] != 0 and distance_stack[i] > distance_stack[next_node] + connection_status[i]:
+                distance_stack[i] = distance_stack[next_node] + connection_status[i]
+
+
+    return distance_stack[to_node]
+
+def dijkstra_one_to_all(adj_matrix, from_node, to_node):
+    """
+    Basic Dijkstra algo implementation
+    :param adj_matrix: adj matrix which contains information related to the graph
+    :param from_node: the source node index
+    :param to_node: the target node index
+    :return: Distance between source and target, which should be the shortest path
+    """
+    close_set = np.zeros(len(adj_matrix))
+    distance_stack = [np.inf for i in range(len(adj_matrix))]
+    distance_stack[from_node] = 0
+    while 0 in close_set:
         next_node = minimum_node(distance_stack,np.inf,close_set)
         connection_status = adj_matrix[next_node]
         for i in range(len(connection_status)):
@@ -43,14 +64,14 @@ if __name__ == '__main__':
                [8, 11, 0, 0, 0, 0, 1, 0, 7],
                [0, 0, 2, 0, 0, 0, 6, 7, 0]
                ]
-    print(dijkstra(g, 0, 1))
-    print(dijkstra(g, 0, 2))
-    print(dijkstra(g, 0, 3))
-    print(dijkstra(g, 0, 4))
-    print(dijkstra(g, 0, 5))
-    print(dijkstra(g, 0, 6))
-    print(dijkstra(g, 0, 7))
-    print(dijkstra(g, 0, 8))
+    print(dijkstra_one_to_one(g, 0, 1))
+    print(dijkstra_one_to_one(g, 0, 2))
+    print(dijkstra_one_to_one(g, 0, 3))
+    print(dijkstra_one_to_one(g, 0, 4))
+    print(dijkstra_one_to_one(g, 0, 5))
+    print(dijkstra_one_to_one(g, 0, 6))
+    print(dijkstra_one_to_one(g, 0, 7))
+    print(dijkstra_one_to_one(g, 0, 8))
 
 
 

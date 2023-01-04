0433d8832d3523d8675820a3380924523361c128
u7079256 <wyrsjz@163.com>
2023-01-04 22:23:10 +1100

    scoring functions

--- Lower_bound_a_star_utils.py
+++ Lower_bound_a_star_utils.py
@@ -1,3 +1,7 @@
+import math
+import numpy as np
+
+
 def scoring_with_avg(feature_dict):
     """
     Calculating the avgerage distance between specific landmark to all other nodes, then passing through sigmoid
@@ -6,8 +10,16 @@ def scoring_with_avg(feature_dict):
     {landmark1:estimated_distance, landmark2:estimated_distance2....}}
     :return: A dict, {landmark1:score1.....}
     """
-    # TODO
-    return
+    landmark_score = {}
+    for key, value in feature_dict:
+        for landmark, distance in value:
+            if landmark not in landmark_score:
+                landmark_score[landmark] = distance
+            else:
+                landmark_score[landmark] += distance
+    for key, value in landmark_score:
+        landmark_score[key] = - math.tanh(value) + 1
+    return landmark_score
 
 
 def scoring_with_ordering(feature_dict):
@@ -20,5 +32,17 @@ def scoring_with_ordering(feature_dict):
     :param feature_dict:
     :return: A dict, {landmark1:score1.....} n >= score_n >= 1
     """
-    # TODO
-    return
+    landmark_score = {}
+    for key, value in feature_dict:
+        distance_list = sorted(value.items(), key=lambda x: x[1], reverse=True)
+        score_default = 0
+        for pairs in distance_list:
+            if pairs[0] not in landmark_score:
+                landmark_score[pairs[0]] = score_default
+                score_default += 1
+            else:
+                landmark_score[pairs[0]] += score_default
+                score_default += 1
+    for key, value in landmark_score:
+        landmark_score[key] = - math.tanh(value) + 1
+    return landmark_score
